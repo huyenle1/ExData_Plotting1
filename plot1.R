99d3b79e31996b1e5b077fd2b9ea03de94fac9a8
huyenle1 <lekhanhhuyen89@gmail.com>
2014-07-13 11:28:13 -0500

    Update plot1.R

--- plot1.R
+++ plot1.R
@@ -8,37 +8,3 @@ data$DateTime <- strptime(paste(data$Date, data$Time),"%d/%m/%Y %H:%M:%S")
 png(filename="plot1.png", width = 480, height = 480)
 hist(data$Global_active_power, col="red", xlab="Global Active Power (kilowatts)", main = "Global Active Power")
 dev.off()
-
-# Create a graph for Global Active Power during the two days - Plot 2
-
-png(filename="plot2.png", width = 480, height = 480)
-plot(data$DateTime, data$Global_active_power, type = "l", xlab = "", ylab = "Global Active Power (kilowatts)")
-dev.off()
-
-# Create a graph for Energy sub metering - Plot 3
-
-png(filename="plot3.png", width = 480, height = 480)
-plot(data$DateTime, data$Sub_metering_1, type = "l", xlab = "", ylab = "Energy sub metering")
-lines(data$DateTime, data$Sub_metering_2, col="red")
-lines(data$DateTime, data$Sub_metering_3, col="blue")
-legend("topright", lty = 1, col = c("black", "red", "blue"), legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
-dev.off()
-
-# Create a set of graphs - Plot 4
-
-png(filename="plot4.png", width = 480, height = 480)
-
-par(mfcol=c(2,2))
-
-plot(data$DateTime, data$Global_active_power, type = "l", xlab = "", ylab = "Global Active Power")
-
-plot(data$DateTime, data$Sub_metering_1, type = "l", xlab = "", ylab = "Energy sub metering")
-lines(data$DateTime, data$Sub_metering_2, col="red")
-lines(data$DateTime, data$Sub_metering_3, col="blue")
-legend("topright", lty = 1, col = c("black", "red", "blue"), legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), bty = "n")
-
-plot(data$DateTime, data$Voltage, type = "l", xlab = "datetime", ylab = "Voltage")
-
-plot(data$DateTime, data$Global_reactive_power, type = "l", xlab = "datetime", ylab = "Global Reactive Power")
-
-dev.off()
